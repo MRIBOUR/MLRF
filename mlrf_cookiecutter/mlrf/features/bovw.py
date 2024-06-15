ce86498c327b85ea1c267eedd583d93345ed3af5
Tristan Dujardin <tristan.dujardin@epita.fr>
2024-06-15 16:58:06 +0200

    feat: added comments for bovw and hog

--- mlrf_cookiecutter/mlrf/features/bovw.py
+++ mlrf_cookiecutter/mlrf/features/bovw.py
@@ -1,8 +1,3 @@
-"""from dataset import load_cifar10_batch"""
-"""from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-from find_cifar import find_cifar"""
-
 from sklearn.cluster import KMeans
 import numpy as np
 import cv2
@@ -10,11 +5,15 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-"""
-Goal of the class is to take a dataset and to output the features as a list
-"""
-class BoVW():
+class BoVW:
     def __init__(self, data, kmeans_clusters=10):
+        """
+        Initializes the Bag of Visual Words (BoVW) class with input data.
+        
+        Parameters:
+        - data (array-like): A list or array of images.
+        - kmeans_clusters (int): The number of clusters for KMeans (visual words).
+        """
         self._data = data
         self._sift = cv2.SIFT_create()
         logger.info('Created SIFT')
@@ -26,99 +25,59 @@ class BoVW():
         self._kmeans_clusters = kmeans_clusters
         self._kmeans = self._train_kmeans(self._kmeans_clusters)
         logger.info('Trained KMeans')
-    
-    def _train_kmeans(self, kmeans_clusters):
-        self._kmeans = KMeans(n_clusters=kmeans_clusters, random_state=42)
-        self._kmeans.fit(self._descriptors)
-    
+
     def _get_img_descriptors(self):
+        """
+        Computes SIFT descriptors for each image in the dataset and stores them.
+        
+        This method processes each image to extract its SIFT keypoints and descriptors,
+        appending the descriptors to the `_descriptors` list.
+        """
         for img in self._data:
-            _, descriptors = sift.detectAndCompute(img, None)
+            _, descriptors = self._sift.detectAndCompute(img, None)
+            
             if descriptors is not None:
                 self._descriptors.append(descriptors)
             else:
                 self._descriptors.append([])
-    
+
+    def _train_kmeans(self, kmeans_clusters):
+        """
+        Trains a KMeans model on the collected descriptors to create visual words.
+        
+        Parameters:
+        - kmeans_clusters (int): The number of clusters (visual words) for KMeans.
+        
+        Returns:
+        - KMeans: The trained KMeans model.
+        """
+        all_descriptors = np.vstack(self._descriptors) if self._descriptors else np.empty((0, 128))
+        
+        kmeans = KMeans(n_clusters=kmeans_clusters, random_state=42)
+        kmeans.fit(all_descriptors)
+        
+        return kmeans
+
     def get_features(self):
+        """
+        Converts image descriptors into feature histograms using the trained KMeans model.
+        
+        This method predicts the closest visual words for each descriptor and 
+        constructs a histogram of visual word occurrences for each image.
+        
+        Returns:
+        - np.ndarray: Array of histograms representing features for each image.
+        """
         histograms = []
-        for desc in self._data:
-            hist, _ = np.histogram(self._kmeans.predict(desc), bins=np.arange(self._kmeans_clusters + 1))
+        
+        for descriptors in self._descriptors:
+            if len(descriptors) == 0:
+                hist = np.zeros(self._kmeans_clusters)
+            else:
+                words = self._kmeans.predict(descriptors)
+                hist, _ = np.histogram(words, bins=np.arange(self._kmeans_clusters + 1))
+            
             histograms.append(hist)
+        
         logger.info('Transformed Features into Histograms')
         return np.array(histograms)
-
-
-"""
-
-def compute_histograms(images, sift, kmeans, num_clusters):
-    histograms = []
-
-    for img in images:
-        keypoints, descriptors = sift.detectAndCompute(img, None)
-        if descriptors is not None:
-            words = kmeans.predict(descriptors)
-            histogram, _ = np.histogram(words, bins=np.arange(num_clusters + 1))
-        else:
-            histogram = np.zeros(num_clusters)
-        histograms.append(histogram)
-
-    return np.array(histograms)
-
-def 
-
-###
-
-dataset_path = find_cifar()
-
-data_batches = []
-labels_batches = []
-
-for i in range(1, 6):
-    batch = load_cifar10_batch(dataset_path / f'data_batch_{i}')
-    data_batches.append(batch[b'data'])
-    labels_batches.append(batch[b'labels'])
-
-X_train = np.concatenate(data_batches)
-y_train = np.concatenate(labels_batches)
-
-test_batch = load_cifar10_batch(dataset_path / 'test_batch')
-X_test = test_batch[b'data']
-y_test = test_batch[b'labels']
- 
-X_train = X_train.reshape((len(X_train), 3, 32, 32)).transpose(0, 2, 3, 1)
-X_test = X_test.reshape((len(X_test), 3, 32, 32)).transpose(0, 2, 3, 1)
-
-x_train_gray = np.array([cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in X_train])
-x_test_gray = np.array([cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in X_test])
-
-
-# SIFT
-sift = cv2.SIFT_create()
-
-descriptors_list = []
-
-for img in x_train_gray:
-    keypoints, descriptors = sift.detectAndCompute(img, None)
-    if descriptors is not None:
-        descriptors_list.append(descriptors)
-
-descriptors = np.vstack(descriptors_list)
-#
-
-N_CLUSTERS = 100
-
-kmeans = KMeans(n_clusters=N_CLUSTERS, random_state=42)
-kmeans.fit(descriptors)
-
-visual_vocabulary = kmeans.cluster_centers_
-
-train_histograms = compute_histograms(x_train_gray, sift, kmeans, N_CLUSTERS)
-test_histograms = compute_histograms(x_test_gray, sift, kmeans, N_CLUSTERS)
-
-svm = SVC(kernel='rbf', random_state=42)
-svm.fit(train_histograms, y_train)
-
-y_pred = svm.predict(test_histograms)
-accuracy = accuracy_score(y_test, y_pred)
-
-print(f'Accuracy: {accuracy * 100:.2f}%')"""
--- mlrf_cookiecutter/mlrf/features/hog.py
+++ mlrf_cookiecutter/mlrf/features/hog.py
@@ -1,33 +1,68 @@
 from skimage.feature import hog
-from skimage import data, color, exposure
+from skimage import color
 import numpy as np
 import logging
 
 logger = logging.getLogger(__name__)
 
-class HoG():
+class HoG:
     def __init__(self, data):
+        """
+        Initializes the HoG class with input data.
+        
+        Parameters:
+        - data (array-like): A list or array of images.
+        """
         self._data = data
         self._tdata = []
         self._features = []
-        logger.info('Created Hog Class')
-    
+        logger.info('Created HoG Class')
+
     def get_features(self):
+        """
+        Extracts HoG features from the input images.
+        
+        Returns:
+        - np.ndarray: Array of HoG features for each image.
+        """
         self._tdata = []
         self._features = []
+
         for img in self._data:
-            if len(img.shape) == 3:
-                image = color.rgb2gray(image)
+            try:
+                if len(img.shape) == 3:
+                    image = color.rgb2gray(img)
+                else:
+                    image = img
 
-            features, timage = hog(image, orientations=9, pixels_per_cell=(32, 32), cells_per_block=(2, 2), visualize=True, multichannel=False)
+                features, timage = hog(
+                    image,
+                    orientations=9,
+                    pixels_per_cell=(32, 32),
+                    cells_per_block=(2, 2),
+                    visualize=True,
+                    multichannel=False
+                )
 
-            self._features.append(features)
-            self._tdata.append(timage)
+                self._features.append(features)
+                self._tdata.append(timage)
+                
+            except Exception as e:
+                logger.error(f'Error processing image: {e}')
+                continue
+
         logger.info('Calculated gradients successfully')
         return np.array(self._features)
-    
+
     def get_images(self):
-        if self._tdata == []:
+        """
+        Returns the transformed images showing HoG visualizations.
+        
+        Returns:
+        - list: List of HoG visualized images.
+        """
+        if not self._tdata:
             logger.info('No gradients found, executing gradient detection')
             self.get_features()
+
         return self._tdata
